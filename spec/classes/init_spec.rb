58703b4dc1ef8fd0c4023f482f9d6a1d82b954d1
Phil Friderici <phil.friderici@ericsson.com>
2018-10-08 10:38:06 +0000

    Refactor tests for Ubuntu
    - use newest version for Ubuntu default tests
    - add specific test cases for Ubuntu versions

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -195,14 +195,14 @@ describe 'nisclient' do
       }
     end
 
-    context 'with defaults params on Ubuntu' do
+    context 'with defaults params on Ubuntu 18.04' do
       let :facts do
         {
           :domain                 => 'example.com',
           :kernel                 => 'Linux',
           :lsbdistid              => 'Ubuntu',
           :osfamily               => 'Debian',
-          :operatingsystemrelease => '14.04',
+          :operatingsystemrelease => '18.04',
         }
       end
 
@@ -252,10 +252,30 @@ describe 'nisclient' do
       it {
         should contain_service('nis_service').with({
           'ensure' => 'running',
-          'name'   => 'ypbind',
+          'name'   => 'nis',
           'enable' => 'true',
         })
       }
+
+      context 'with version 14.04' do
+        let :facts do
+          {
+            :domain                 => 'example.com',
+            :kernel                 => 'Linux',
+            :lsbdistid              => 'Ubuntu',
+            :osfamily               => 'Debian',
+            :operatingsystemrelease => '14.04',
+          }
+        end
+
+        it {
+          should contain_service('nis_service').with({
+            'ensure' => 'running',
+            'name'   => 'ypbind',
+            'enable' => 'true',
+          })
+        }
+      end
 
       context 'with version 16.04' do
         let :facts do
